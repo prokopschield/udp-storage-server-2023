8d6ce733e35044b2bd6ba34785e830599807805f
Prokop <prokop.schield@protonmail.com>
2023-12-23 23:40:58 +0100

    refactor(src/modules/mapping/mod.rs): improve MemoryMapping model

--- src/modules/mapping/mod.rs
+++ src/modules/mapping/mod.rs
@@ -1,34 +1,50 @@
-pub enum MemoryMapping {
-    ReadOnly(std::sync::Arc<std::sync::Mutex<memmap::Mmap>>),
-    ReadWrite(std::sync::Arc<std::sync::Mutex<memmap::MmapMut>>),
+use super::error::{to_error, UssResult};
+
+pub struct MemoryMapping<'a> {
+    owned_ro: Option<std::sync::Arc<std::sync::Mutex<memmap::Mmap>>>,
+    owned_rw: Option<std::sync::Arc<std::sync::Mutex<memmap::MmapMut>>>,
+    roref: &'a [u8],
 }
 
-pub fn create_ro_mapping(file_path: &str) -> std::io::Result<MemoryMapping> {
+pub fn create_ro_mapping(file_path: &str) -> UssResult<MemoryMapping> {
     let file = std::fs::OpenOptions::new()
         .read(true)
         .write(false)
         .create(false)
-        .open(file_path)?;
+        .open(file_path)
+        .map_err(to_error)?;
 
-    let mmap = unsafe { memmap::MmapOptions::new().map(&file)? };
+    let mmap = unsafe { memmap::MmapOptions::new().map(&file).map_err(to_error)? };
 
-    return Ok(MemoryMapping::ReadOnly(std::sync::Arc::from(
-        std::sync::Mutex::from(mmap),
-    )));
+    let slice = &mmap[..];
+    let arc = std::sync::Arc::from(std::sync::Mutex::from(mmap));
+
+    return Ok(MemoryMapping {
+        owned_ro: Some(arc),
+        owned_rw: None,
+        roref: slice,
+    });
 }
 
-pub fn create_rw_mapping(file_path: &str) -> std::io::Result<MemoryMapping> {
+pub fn create_rw_mapping(file_path: &str) -> UssResult<MemoryMapping> {
     let file = std::fs::OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
-        .open(file_path)?;
+        .open(file_path)
+        .map_err(to_error)?;
 
-    let mmap = unsafe { memmap::MmapOptions::new().map(&file)? };
+    let mmap = unsafe { memmap::MmapOptions::new().map(&file).map_err(to_error)? };
 
-    let mmut = mmap.make_mut()?;
+    let mmut = mmap.make_mut().map_err(to_error)?;
 
-    return Ok(MemoryMapping::ReadWrite(std::sync::Arc::from(
-        std::sync::Mutex::from(mmut),
-    )));
+    let slice = &mmap[..];
+    let mutex = std::sync::Mutex::from(mmut);
+    let arc = std::sync::Arc::from(mutex);
+
+    Ok(MemoryMapping {
+        owned_ro: None,
+        owned_rw: Some(arc),
+        roref: slice,
+    })
 }
