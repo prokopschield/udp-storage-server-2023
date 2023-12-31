5f962d677588ce008b2a703f6402a467ffd97e9b
Prokop <prokop.schield@protonmail.com>
2024-01-01 00:06:46 +0100

    feat(src/modules/tree/node.rs): Node::new()

--- src/modules/tree/node.rs
+++ src/modules/tree/node.rs
@@ -1,7 +1,10 @@
-use serde::{de::DeserializeOwned, Serialize};
-use std::rc::Rc;
-
 use super::*;
+use crate::store::DataLake;
+use serde::{de::DeserializeOwned, Serialize};
+use std::{
+    rc::Rc,
+    sync::{Arc, Mutex},
+};
 
 #[derive(Clone)]
 pub enum NodeChild<K: Serialize + DeserializeOwned, V: Serialize + DeserializeOwned> {
@@ -19,4 +22,27 @@ pub struct NodeEntry<K: Serialize + DeserializeOwned, V: Serialize + Deserialize
 pub struct Node<K: Serialize + DeserializeOwned, V: Serialize + DeserializeOwned> {
     depth: usize,
     entries: Vec<NodeEntry<K, V>>,
+    lake: Arc<Mutex<DataLake>>,
+}
+
+impl<K: Serialize + DeserializeOwned, V: Serialize + DeserializeOwned> Node<K, V> {
+    pub fn new_from_props(
+        lake: Arc<Mutex<DataLake>>,
+        depth: usize,
+        entries: Vec<NodeEntry<K, V>>,
+    ) -> Self {
+        Self {
+            depth,
+            entries,
+            lake,
+        }
+    }
+
+    pub fn new_with_depth(lake: Arc<Mutex<DataLake>>, depth: usize) -> Self {
+        Self::new_from_props(lake, depth, vec![])
+    }
+
+    pub fn new(lake: Arc<Mutex<DataLake>>) -> Self {
+        Self::new_with_depth(lake, 0)
+    }
 }
