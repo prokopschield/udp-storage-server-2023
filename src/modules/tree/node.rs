96e430ed1bf75c714e303d274049fa45e4fb67cb
Prokop <prokop.schield@protonmail.com>
2023-12-31 22:51:42 +0100

    chore(src/modules/tree/node.rs): derive(Clone)

--- src/modules/tree/node.rs
+++ src/modules/tree/node.rs
@@ -3,16 +3,19 @@ use std::rc::Rc;
 
 use super::*;
 
+#[derive(Clone)]
 pub enum NodeChild<K: Serialize + DeserializeOwned, V: Serialize + DeserializeOwned> {
     Node(Rc<Node<K, V>>),
     Leaf(Rc<Leaf<K, V>>),
 }
 
+#[derive(Clone)]
 pub struct NodeEntry<K: Serialize + DeserializeOwned, V: Serialize + DeserializeOwned> {
     key: u32,
     child: NodeChild<K, V>,
 }
 
+#[derive(Clone)]
 pub struct Node<K: Serialize + DeserializeOwned, V: Serialize + DeserializeOwned> {
     depth: usize,
     entries: Vec<NodeEntry<K, V>>,
